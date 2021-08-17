7bfcfd3e130e6d42c074b5f9ebca3c1b0c4909a4
Majed <khreimmajed@gmail.com>
2021-08-17 20:50:37 +0200

    create Progression model

---
+++ app/Models/Progression.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Progression extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'progress_value',
+        'rate',
+        'progress_date'
+    ];
+
+    public function cryptocurrency() {
+        return $this->belongsTo(Cryptocurrency::class);
+    }
+
+
+}
