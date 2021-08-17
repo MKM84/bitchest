19e14cd88388f347ea1d8df8ed90df7c3452da1d
Majed <khreimmajed@gmail.com>
2021-08-17 21:13:01 +0200

    create Transaction model

---
+++ app/Models/Transaction.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Transaction extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'total_solde',
+        'quantity',
+        'state',
+        'purchase_date',
+        'selling_date',
+        'purchase_price',
+        'selling_price'
+    ];
+
+    public function user() {
+        return $this->hasOne(User::class);
+    }
+
+    public function cryptocurrency() {
+        return $this->hasOne(Cryptocurrency::class);
+    }
+
+    public function progression() {
+        return $this->hasOne(Progression::class);
+    }
+}
