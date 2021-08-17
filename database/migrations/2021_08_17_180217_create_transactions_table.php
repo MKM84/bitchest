fb6bcc96b984f093eda94ce7204082e332f06810
Ludo <ludovic.charlier@hotmail.fr>
2021-08-17 20:18:48 +0200

    create transactions_table

---
+++ database/migrations/2021_08_17_180217_create_transactions_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTransactionsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('transactions', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedDecimal("total_solde")->default(0);
+            $table->unsignedDecimal("quantity");
+            $table->boolean("state")->default(0);
+            $table->datetime("purchase_date");
+            $table->datetime("selling_date");
+            $table->unsignedDecimal("purchase_price");
+            $table->unsignedDecimal("selling_price")->nullable();
+
+            $table->foreignId("users_id")->constrained("users")->onDelete("cascade");
+            $table->foreignId("cryptocurrency_id")->constrained("cryptocurrencies");
+            $table->foreignId("progression_id")->constrained("progressions");
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('transactions');
+    }
+}
