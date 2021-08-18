43e1a4a6f533f874bc3828799ca18aaee16de134
Majed <khreimmajed@gmail.com>
2021-08-18 21:16:20 +0200

    calculate sum in case of sellig or purchase

--- database/seeders/TransactionsTableSeeder.php
+++ database/seeders/TransactionsTableSeeder.php
@@ -18,26 +18,30 @@ class TransactionsTableSeeder extends Seeder
     {
         \App\Models\Transaction::factory(100)->create();
 
-        // Find users that have admin status
 
+        // Delete users that have admin status
         $admins = DB::table('users')->where('status', 'admin')->get('id')->toArray();
 
-        // The Arr::pluck method retrieves all of the values for a given key from an array:
+        // The Arr::pluck method retrieves all of the values for a given key from an array
         $admins_ids = Arr::pluck($admins, 'id');
 
-        // The whereIn method verifies that a given column's value is contained within the given array:
+        // The whereIn method verifies that a given column's value is contained within the given array
         DB::table('transactions')
             ->whereIn('user_id', $admins_ids)
             ->delete();
 
-
-         DB::table('transactions')
+        // Set the selling price & date to null if there's no selling operation
+        DB::table('transactions')
             ->where('state', false)
             ->update([
                 'selling_price'    => null,
                 'selling_date'     => null
             ]);
 
+        // Calculate the sum in case of selling
+        DB::update("UPDATE `transactions` SET `sum` = `quantity` * `selling_price` WHERE `state` = 1");
 
+        // Calculate the sum in case of purchase
+        DB::update("UPDATE `transactions` SET `sum` = `quantity` * `purchase_price` WHERE `state` = 0");
     }
 }
