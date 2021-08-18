1717b113f02dd9dc3a83d9114e13f051068362f7
Ludo <ludovic.charlier@hotmail.fr>
2021-08-18 11:46:36 +0200

    Test

--- database/seeders/ProgressionsTableSeeder.php
+++ database/seeders/ProgressionsTableSeeder.php
@@ -14,54 +14,49 @@ class ProgressionsTableSeeder extends Seeder
      *
      * @return void
      */
-    
-    
+
+
     public function run()
     {
         //
-        function getCotationFor($cryptoname){	
-            return ((rand(0, 99)>40) ? 1 : -1) * ((rand(0, 99)>49) ? ord(substr($cryptoname,0,1)) : ord(substr($cryptoname,-1))) * (rand(1,10) * .01);
+        function getCotationFor($cryptoname)
+        {
+            return ((rand(0, 99) > 40) ? 1 : -1) * ((rand(0, 99) > 49) ? ord(substr($cryptoname, 0, 1)) : ord(substr($cryptoname, -1))) * (rand(1, 10) * .01);
         }
 
         $cryptocurrencies = \App\Models\Cryptocurrency::all();
 
         $date_now = now();
-        $date_initial = date('Y-m-d', strtotime($date_now. ' - 30 days'));
+        $date_initial = date('Y-m-d', strtotime($date_now . ' - 30 days'));
 
-        foreach ($cryptocurrencies as $crypto)
-        {
-            $current_value=$crypto->current_value;
+        foreach ($cryptocurrencies as $crypto) {
+            $current_value = $crypto->current_value;
             $cryptoname = $crypto->name;
-            
 
-        for($i=0;$i<30;$i++){
-            
-            $date=date('Y-m-d', strtotime($date_initial. ' + '.($i+1).' days'));
+            $progress_value = $current_value;
 
-            //$date = date('Y-m-d', time() + (($i*86400)));
-            $rate=getCotationFor($cryptoname)*1000;
-            if($i==0){
-                $progress_value=$current_value;-
-                $rate="0";
+            for ($i = 0; $i < 30; $i++) {
+
+                $date = date('Y-m-d', strtotime($date_initial . ' + ' . ($i + 1) . ' days'));
+
+
+                $rate = getCotationFor($cryptoname) * 1000;
+                if ($i == 0) {
+                    $progress_value = $current_value;
+                    $rate = "0";
+                } else {
+                    $progress_value += $rate;
+                }
+
+                DB::table('progressions')->insert([
+                    [
+                        'progress_value' => $progress_value,
+                        'rate' => $rate,
+                        'progress_date' => $date,
+                        'cryptocurrency_id' => $crypto->id,
+                    ]
+                ]);
             }
-            else{
-                $progress_value=$progress_value+$rate;
-            }
-            
-            DB::table('progressions')->insert([
-                [
-                   // 'progress_value' => DB::table('cryptocurrencies')->where('current_value',  $x),
-                   'progress_value' => $progress_value,
-                    'rate' => $rate,
-                    'progress_date'=>$date,
-                    'cryptocurrency_id'=>$crypto->id,
-                ]
-            ]);
-
         }
-
-}
-
-
     }
 }
