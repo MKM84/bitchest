89e3668556a75da53af23a166a7b40e10c71c939
Majed <khreimmajed@gmail.com>
2021-08-23 00:30:32 +0200

    Create the login component page and create login route

---
+++ bitchest-front/src/pages/Login.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="home col-4 mx-auto py-5 mt-5 bg-light p-5 rounded">
+    <h2 class="text-center mb-4">Login</h2>
+
+    <form>
+      <div class="mb-3">
+        <label for="exampleInputEmail1" class="form-label">Email :</label>
+        <input
+          type="email"
+          class="form-control"
+          id="email"
+          aria-describedby="emailHelp"
+          v-model="form.email"
+        />
+        <div id="emailHelp" class="form-text text-danger" v-if="errors.email" >
+           {{ errors.email[0] }}
+        </div>
+      </div>
+      <div class="mb-3">
+        <label for="exampleInputPassword1" class="form-label">Password : </label>
+        <input type="password" class="form-control" id="exampleInputPassword1" />
+        <div id="emailHelp" class="form-text text-danger" v-if="errors.password">
+          {{ errors.password[0] }}
+        </div>
+      </div>
+
+      <button @click.prevent="login" class="btn btn-primary">Login</button>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Login",
+  data() {
+    return {
+      form: {
+        email: "",
+        name: "",
+      },
+      errors: []
+    };
+  },
+  methods: {
+      login() {
+
+      }
+  },
+};
+</script>
